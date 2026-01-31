f8be5efc25e18688a5336b97849f471c8d582bb9
ABDULAZIZ ALRASHEDI <bogico.sa@gmail.com>
2026-02-01 02:22:00 +0300

    🔥 Integrate Firebase Realtime Database

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,16 @@
 // إعدادات Firebase
 // ملاحظة: استبدل هذه الإعدادات بإعدادات مشروعك من Firebase Console
 const firebaseConfig = {
-    apiKey: "AIzaSyDemoKeyForHotelSystem123456789",
-    authDomain: "hotel-system-demo.firebaseapp.com",
-    databaseURL: "https://hotel-system-demo-default-rtdb.firebaseio.com",
-    projectId: "hotel-system-demo",
-    storageBucket: "hotel-system-demo.appspot.com",
-    messagingSenderId: "123456789012",
-    appId: "1:123456789012:web:abcdef123456"
+    apiKey: "AIzaSyAvOrqb1GNtrLUFuF3rE5c-nBeVNlkDHIA",
+    authDomain: "hotel-system-f50a4.firebaseapp.com",
+    databaseURL: "https://hotel-system-f50a4-default-rtdb.firebaseio.com",
+    projectId: "hotel-system-f50a4",
+    storageBucket: "hotel-system-f50a4.firebasestorage.app",
+    messagingSenderId: "671973425461",
+    appId: "1:671973425461:web:ff3d3fc7eae32a5a214c7c",
+    measurementId: "G-4XZ3T2MWHN"
+
+    
 };
 
 // تهيئة Firebase
